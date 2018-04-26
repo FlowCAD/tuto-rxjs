c562d0d0a010592bee951483a39cf483945d5015
FlowCAD <florian.cadoz@gmail.com>
2018-04-26 11:32:57 +0200

    Simple sample of observer/observable relationship

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,14 @@
-import * as Rx from "rxjs/Observable"
+import { Observable } from "rxjs/Observable"
 
-console.log(Rx)
+let observable = Observable.create( (observer : any) => {
+    observer.next('Hey mec !')
+})
+
+let addItem = ((val:any) => {
+    let node = document.createElement("li")
+    let textnode = document.createTextNode(val)
+    node.appendChild(textnode)
+    document.getElementById("output").appendChild(node)
+})
+
+observable.subscribe((x:any) => addItem(x))
